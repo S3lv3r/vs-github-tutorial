0793925d65603a6170655dbdecbc412946ed630f
S3lv3r <131556687+S3lv3r@users.noreply.github.com>
2025-03-29 22:07:28 -0600

    cambio

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,9 +47,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(310, 68);
             label1.Name = "label1";
-            label1.Size = new Size(47, 20);
+            label1.Size = new Size(203, 20);
             label1.TabIndex = 1;
-            label1.Text = "Titulo";
+            label1.Text = "Repositorio Segunda Persona";
             // 
             // Form1
             // 
