487fd9ca01a56242be8b740377fcc8db45788495
S3lv3r <131556687+S3lv3r@users.noreply.github.com>
2025-03-29 22:47:28 -0600

    Cambio por clone

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("No, ahora quiero esto");
+            MessageBox.Show("Cambio por clone");
         }
     }
 }
